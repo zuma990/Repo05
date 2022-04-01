841a1e8955e020e8e79b9497d32d82429af23bca
zuma990 <zumajuve@gmail.com>
2022-04-01 14:43:03 +0200

    Added new file

---
+++ ex07/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line
+  
+    var myName = "Mistral";
+    var myString = "My name is " + myName + " and I am well!";
+    
+    // only change code above this line
+  
+    return myString;
+  }
+  console.log(main());
+  module.exports = main;
+  
