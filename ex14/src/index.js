b276f0708f6e0555d786db4b189bb99908aa81f8
zuma990 <zumajuve@gmail.com>
2022-04-01 22:01:09 +0200

    Added new file

---
+++ ex14/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+
+    // only change code below this line
+
+    var lastName = "Bezos";
+    var thirdToLastLetterOfLastName = lastName[lastName.length - 3];
+    
+
+    // only change code above this line
+  
+  
+    return thirdToLastLetterOfLastName;
+}
+console.log(main());
+module.exports = main;
