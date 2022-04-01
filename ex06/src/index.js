5c90e2d60c47f79b0ea71e3ccdb68a0841ee1326
zuma990 <zumajuve@gmail.com>
2022-04-01 14:35:49 +0200

    Added new file

---
+++ ex06/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // Only change code below this line
+  
+   var myString = "Concatenation is the process of appending  ";
+    myString+= "one string to the end of another string.";
+    
+    // only change code above this line
+  
+    return myString;
+    
+  }
+  console.log(main());
+  module.exports = main;
+  
