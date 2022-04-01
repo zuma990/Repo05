5c9221d7ec6296e78f48b1b233ce07855893bd6f
zuma990 <zumajuve@gmail.com>
2022-04-01 11:34:06 +0200

    Added new file

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+  // Only change code below this line
+
+  var myFirstString = "I think therefore I \"am\" --sentient.";
+  var mySecondString = "No, you only \"think\", so you aren't.";
+
+  // only change code above this line
+
+  return {
+    myFirstString,
+    mySecondString,
+  };
+}
+console.log(main());
+module.exports = main;
