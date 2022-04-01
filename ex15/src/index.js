98ceb9712a94360c69c688e5ecb3d5c887ad0461
zuma990 <zumajuve@gmail.com>
2022-04-01 22:38:48 +0200

    Added new file

---
+++ ex15/src/index.js
@@ -0,0 +1,19 @@
+function main(){
+
+    // only change code below this line
+
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+
+    var wordBlanks = "I have a " + myAdjective + " " + myNoun + " and he can " + myVerb + " " + myAdverb + ".";
+    
+
+    // only change code above this line
+  
+  
+    return wordBlanks;
+}
+console.log(main());
+module.exports = main;
