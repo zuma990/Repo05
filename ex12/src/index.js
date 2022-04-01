b035166c0d3171c4f66f3b502242703461345f9a
zuma990 <zumajuve@gmail.com>
2022-04-01 21:51:55 +0200

    Added new file

---
+++ ex12/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+
+    // only change code below this line
+
+    var lastName = "Tesla";
+    var thirdLetterOfLastName = lastName[2];
+    
+
+    // only change code above this line
+  
+  
+    return thirdLetterOfLastName;
+}
+console.log(main());
+module.exports = main;
