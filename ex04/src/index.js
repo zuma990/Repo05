4678b9a41f20eef525b501d5de281989f46381c7
zuma990 <zumajuve@gmail.com>
2022-04-01 14:23:09 +0200

    Added new file

---
+++ ex04/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this line
+  
+    var myString = "FirstLine\n\tSecondLine\nThirdLine";
+  
+    // only change code above this line
+  return myString;
+   
+  }
+  console.log(main());
+  module.exports = main;
+  
