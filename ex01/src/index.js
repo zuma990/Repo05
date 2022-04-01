719d18c52c4a4eb62120ed85949cb4592430f1f0
zuma990 <zumajuve@gmail.com>
2022-04-01 10:07:14 +0200

    Added new file

---
+++ ex01/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+  // Only change code below this line
+
+  var myFirstName = "Adis";
+  var myLastName = "Tipura";
+  var myHomeTown = "Mostar";
+
+  // only change code above this line
+
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown,
+  };
+}
+console.log(main());
+module.exports = main;
