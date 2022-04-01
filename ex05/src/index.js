884ccee8c2f0d7eac2621f90d563bff4c27dee69
zuma990 <zumajuve@gmail.com>
2022-04-01 14:28:23 +0200

    Added new file

---
+++ ex05/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this line
+  var myString = "This is the first string. " + "This is the second string.";
+    
+    // only change code above this line
+  
+    return myString;
+    
+  }
+  console.log(main());
+  module.exports = main;
+  
